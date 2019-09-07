7a7b0f2c38ccf7c84bb5116a5aa1e23f1c6f7745
Matt Casanova <lazersquad@gmail.com>
2019-09-07 11:16:41 -0700

    Completed Ep 8

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
+Vue.component('task-list', {
+    template: `
+        <div>
+            <task v-for="task in tasks">{{ task.task }}</task>
+        </div>
+    `,
+
+    data() {
+        return {
+            tasks: [
+                { task: 'Go to the Store', completed: false },
+                { task: 'Go to the post office', completed: true },
+                { task: 'Go to the farm', completed: false },
+                { task: 'Go to the work', completed: true },
+                { task: 'Go to the other store', completed: false },
+            ]
+        }
+    }
+
+});
+
+
 Vue.component('task', {
-
     template: '<li><slot></slot></li>'
-
-
 });
 
 new Vue({
