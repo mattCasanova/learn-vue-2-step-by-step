0282fa67850007e483f9866d0137fd241f26ad75
Matt Casanova <matt.c@vshred.com>
2019-09-12 09:48:36 -0700

    Completed Ep 22

---
+++ Vue-Forms/resources/assets/js/app.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import axios from 'axios';
+import Form from './core/Form';
+
+import Example from './components/Example'
+
+window.axios = axios;
+window.From = Form;
+
+new Vue({
+    el: '#app',
+
+    components: {
+        Example
+    },
+
+    data: {
+        form: new Form({
+            name: '',
+            description: ''
+        })
+    },
+
+    methods: {
+        onSubmit() {
+            this.form.post('/projects')
+                .then(response => alert('Wahoo!'));
+        }
+    }
+});
